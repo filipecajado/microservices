b0e0a7f5bcc4ffc64879442de6dc3ed9fc2fbdd3
Filipe Cajado <filipecajado@ucl.br>
2023-09-08 09:15:44 -0300

    add transportador

--- loja/src/main/java/br/com/alura/microservice/loja/model/Compra.java
+++ loja/src/main/java/br/com/alura/microservice/loja/model/Compra.java
@@ -1,17 +1,65 @@
 package br.com.alura.microservice.loja.model;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Compra {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
 	private Long pedidoId;
 	
 	private Integer tempoDePreparo;
 	
 	private String enderecoDestino;
+	
+	private Long voucher;
+	
+	private LocalDate dataParaEntrega;
+	
+	@Enumerated(EnumType.STRING)
+	private CompraState state;
+
+	public CompraState getState() {
+		return state;
+	}
+
+	public void setState(CompraState state) {
+		this.state = state;
+	}
+
+	public Long getVoucher() {
+		return voucher;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setVoucher(Long voucher) {
+		this.voucher = voucher;
+	}
+
+	public LocalDate getDataParaEntrega() {
+		return dataParaEntrega;
+	}
+
+	public void setDataParaEntrega(LocalDate dataParaEntrega) {
+		this.dataParaEntrega = dataParaEntrega;
+	}
 
 	public Long getPedidoId() {
 		return pedidoId;
--- loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
+++ loja/src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -1,66 +1,78 @@
 package br.com.alura.microservice.loja.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.client.TransportadorClient;
 import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.controller.dto.InfoEntregaDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
 import br.com.alura.microservice.loja.controller.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.controller.dto.VoucherDTO;
 import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.model.CompraState;
 import br.com.alura.microservice.loja.repository.CompraRepository;
 
 @Service
 public class CompraService {
-	
-	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
-	
+
 	@Autowired
 	private FornecedorClient fornecedorClient;
-	
+
+	@Autowired
+	private TransportadorClient transportadorClient;
+
 	@Autowired
 	private CompraRepository compraRepository;
-	
-	@HystrixCommand
-	public Compra getById(Long id) {
 
+	@HystrixCommand(threadPoolKey = "getByIdThreadPool")
+	public Compra getById(Long id) {
 		return compraRepository.findById(id).orElse(new Compra());
 	}
 
-	@HystrixCommand(fallbackMethod = "realizaCompraFallback")
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback", threadPoolKey = "realizaCompraThreadPool")
 	public Compra realizaCompra(CompraDTO compra) {
 		
 		final String estado = compra.getEndereco().getEstado();
-		
-		LOG.info("buscando informações do fornecedor de {}", estado);
-		
 		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
-
-		LOG.info("realizando um pedido");
 		
-		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
-		System.out.println(info.getEndereco());
 		Compra compraSalva = new Compra();
+		compraSalva.setEnderecoDestino(info.getEndereco());
+		compraSalva.setState(CompraState.RECEBIDO);
+		compraRepository.save(compraSalva);
+
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
 		compraSalva.setPedidoId(pedido.getId());
 		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
-		compraSalva.setEnderecoDestino(info.getEndereco());
-		
+		compraSalva.setState(CompraState.PEDIDO_REALIZADO);
 		compraRepository.save(compraSalva);
-		
+
+
+		InfoEntregaDTO entregaDTO = new InfoEntregaDTO();
+		entregaDTO.setPedidoId(pedido.getId());
+		entregaDTO.setDataParaEntrega(LocalDate.now().plusDays(pedido.getTempoDePreparo()));
+		entregaDTO.setEnderecoOrigem(info.getEndereco());
+		entregaDTO.setEnderecoDestino(compra.getEndereco().getEstado());
+		VoucherDTO voucher = transportadorClient.reservaEntrega(entregaDTO);
+		compraSalva.setDataParaEntrega(voucher.getPrevisaoParaEntrega());
+		compraSalva.setVoucher(voucher.getNumero());
+		compraSalva.setState(CompraState.RESERVA_ENTREGA_REALIZADA);
+		compraRepository.save(compraSalva);
+
+
 		return compraSalva;
 	}
-	
+
 	public Compra realizaCompraFallback(CompraDTO compra) {
 		Compra compraFallback = new Compra();
 		compraFallback.setEnderecoDestino(compra.getEndereco().toString());
-		
+
 		return compraFallback;
 	}
-
-
 
 }
